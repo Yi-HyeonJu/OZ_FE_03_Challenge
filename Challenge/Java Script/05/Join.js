a25421846a9cf2ea34cab83b89459e5e12d7d8f0
Yi-HyeonJu <guswwn1925@gmail.com>
2024-04-08 13:07:12 +0900

    feat : js 기능을 넣은 회원가입 폼 작성

---
+++ Challenge/Java Script/05/Join.js
@@ -0,0 +1,59 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    let userId = event.target.id.value
+    let userPw1 = event.target.pw1.value
+    let userPw2 = event.target.pw2.value
+    let userName = event.target.name.value
+    let userPhone = event.target.phone.value
+    let userPosition = event.target.position.value
+    let userGender = event.target.gender.value
+    let userEmail = event.target.email.value
+    let userIntro = event.target.intro.value
+    
+    if(userId.length < 6){
+        alert("아이디가 너무 짧습니다. 6자리 이상 입력해주세요.")
+        return
+    }
+    
+    if(userPw1 !== userPw2){
+        alert("비밀번호가 일치하지 않습니다.")
+        return
+    }
+    
+    document.body.innerHTML = ""
+    
+    const div = document.createElement("div")
+    div.style.width = "450px"
+    div.style.height = "300px"
+    div.style.margin = "60px auto 0"
+    div.style.padding = "25px"
+    div.style.borderRadius = "15px"
+    div.style.backgroundColor = "rgba(240, 228, 159, 0.479)"
+    document.body.appendChild(div)
+    
+    const h1 = document.createElement("h1")
+    h1.style.fontSize = "30px"
+    h1.style.paddingBottom = "20px"
+    h1.textContent = `${userId}님 환영합니다.`
+    
+    const p1 = document.createElement("p")
+    p1.textContent = `회원가입 시 입력하신 내역은 다음과 같습니다.`
+    
+    const p2 = document.createElement("p")
+    p2.textContent = `아이디 : ${userId}`
+    
+    const p3 = document.createElement("p")
+    p3.textContent = `이름 : ${userName} `
+    
+    const p4 = document.createElement("p")
+    p4.textContent = `전화번호 : ${userPhone}`
+    
+    const p5 = document.createElement("p")
+    p5.textContent = `원하는 직무 : ${userPosition}`
+    
+    
+    div.append(h1, p1, p2, p3, p4, p5)
+})
