a3e31f0d1e37cbe61ef436f1f9b97d34644d4f0e
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-08 18:16:21 +0900

    feat : item 컴포넌트 만들기

--- Challenge/React/02/src/components/ExpenseItem.js
+++ Challenge/React/02/src/components/ExpenseItem.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import './ExpenseItem.css'
 
 class ExpenseItem extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <li className='item'>
+        <div className='info'>
+          <span className='expense'>빵</span>
+          <span className='amount'>100원</span>
+        </div>
+        <div>
+          <button className='edit-btn'>수정</button>
+          <button className='clear-btn'>삭제</button>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default ExpenseItem;
