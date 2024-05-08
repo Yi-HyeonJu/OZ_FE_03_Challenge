6a87f213681afdfc2237ee71950efd6dd370a6e7
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-08 17:51:46 +0900

    feat : 내부 컴포넌트 만들기

---
+++ Challenge/React/02/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExpenseItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ExpenseItem;
