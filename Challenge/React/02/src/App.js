d6c96edee83cb659326cf7e4f66a6bf00218ed6c
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-08 17:52:23 +0900

    feat : 배경이 되는 컴포넌트 만들기

--- Challenge/React/02/src/App.js
+++ Challenge/React/02/src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import './App.css';
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
 export default class App extends Component {
   render() {
@@ -10,10 +12,12 @@ export default class App extends Component {
           
           <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
             {/*Expensee Form*/}
+            <ExpenseForm/>
           </div>
           
           <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
             {/*Expense List*/}
+            <ExpenseList/>
           </div>
           
           <div style={{display: 'flex', justifyContent: 'start', marginTop: '1rem'}}>
