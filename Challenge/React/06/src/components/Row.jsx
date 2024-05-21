038fe29f11f87f734f36e7658941b9685ee1b3dc
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-21 13:52:48 +0900

    feat : 영화 목록 Row 컴포넌트, CSS 작성

--- Challenge/React/06/src/components/Row.jsx
+++ Challenge/React/06/src/components/Row.jsx
@@ -1,9 +1,54 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState, useCallback } from 'react';
 import './Row.css';
+import axios from '../api/axios';
 
-const Row = () => {
+const Row = ({title, id, fetchUrl}) => {
+
+    const [movies, setMovies] = useState([])
+
+    const fetchMovieData = useCallback(async () => {
+        const response = await axios.get(fetchUrl)
+        setMovies(response.data.results)
+    }, [fetchUrl])
+
+    useEffect(() => {
+        fetchMovieData()
+    }, [fetchMovieData])
+    
+    
+
     return (
         <div>
-            
+            <h2>{title}</h2>
+            <div className='slider'>
+                <div className='slider_arrow-left'>
+                    <span className='arrow' onClick={
+                        () => {document.getElementById(id).scrollLeft -= window.innerWidth -80;
+                            }}
+                        >
+                        {"<"}</span>
+                </div>
+                <div id={id} className='row_posters'>
+                    {movies.map((movie) => {
+                        return(
+                            <img
+                            key={movie.id}
+                            className='row_poster'
+                            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                            alt={movie.name} 
+                            />
+                        )
+                    })}
+                </div>
+                <div className='slider_arrow-right'>   
+                    <span className='arrow' onClick={
+                        () => {document.getElementById(id).scrollLeft += window.innerWidth -80;
+                            }}
+                        >
+                        {">"}</span>
+                </div>
+            </div>
         </div>
     );
 };
