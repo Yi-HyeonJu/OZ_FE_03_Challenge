b0de465d46d558b17c6b760c248c106d777e937e
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-22 10:57:07 +0900

    feat : 한글자 입력시 페이지 이동 오류 수정 & 오타 수정

--- Challenge/React/06/src/pages/SearchPage/index.jsx
+++ Challenge/React/06/src/pages/SearchPage/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom"
+import axios from '../../api/axios';
 
 const SearchPage = () => {
 
@@ -16,59 +16,58 @@ const SearchPage = () => {
     const searchTerm = query.get('q')
 
     useEffect(() => {
-        if(searchTerm) {
-            fetchSearchMoive(searchTerm)
+        if (searchTerm) {
+            fetchSearchMovie(searchTerm);
         }
     }, [searchTerm])
 
-    const fetchSearchMoive = async (searchTerm) => {
-        try{
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
             const response = await axios.get(
                 `/search/multi?include_adult=false&query=${searchTerm}`
             )
-            setSearchResults(response.data.results)
-        }   catch (error) {
-            console.log(error)
+            console.log(response);
+            setSearchResults(response.data.results);
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    if(searchResults.length > 0){
+    if (searchResults.length > 0) {
         return (
-            <section className="search-container">
-                {searchResults.map((movie) => {
-                    if(movie.backdrop_path !== null && movie.media_type !== "perdon"){
-                        const movieImageURL = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
-                        return (
-                            <div className="movie" key={movie.id}>
-                                <div
-                                    onClick={() => navigate(`/${movie.id}`)}
-                                    className="movie_column-poster"
-                                >
-                                    <img
-                                        src={movieImageURL}
+        <section className="search-container">
+            {searchResults.map((movie) => {
+                if (movie.backdrop_path !== null && movie.media_type !== "person") {
+                    const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+                    return (
+                        <div className="movie" key={movie.id}>
+                            <div
+                                onClick={() => navigate(`/${movie.id}`)}
+                                className="movie_column-poster"
+                            >
+                            <img
+                                        src={movieImageUrl}
                                         alt="영화 이미지"
-                                        className="movie_poster"
-                                    />
-                                </div>
+                                className="movie_poster"
+                            />
                             </div>
-                        )
-                    
-                    }
-                })}
-            </section>
-        );
+                        </div>
+                    )
+                }
+            })}
+        </section>
+        )
     } else {
         return (
             <section className="no-results">
                 <div className="no-results_text">
-                    <p>
+                <p>
                         {searchTerm} 검색어에 해당하는 영화가 없습니다. 
-                    </p>
-                </div>
+                </p>
+            </div>
             </section>
         )
     }
+}
 
-};
-
-export default SearchPage;
+export default SearchPage
