9ba50acfa69ea4e2300ef77b55bb775ac913f27b
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-22 14:19:19 +0900

    feat : 디테일 페이지 컴포넌트 작성

--- Challenge/React/06/src/pages/DetailPage/index.jsx
+++ Challenge/React/06/src/pages/DetailPage/index.jsx
@@ -1,9 +1,52 @@
+import axios from "../../api/axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+
 const DetailPage = () => {
+
+    const { movieId } = useParams()
+    const [ movie, setMovie ] = useState(null)
+
+    useEffect(() => {
+        async function fetchData(){
+            try {  
+                const response = await axios.get(`/movie/${movieId}`)
+                setMovie(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [movieId])
+
+    if (!movie) return null
+    
     return (
-        <div>
-            DetailPage
-        </div>
+        <section>
+            <StyledImage
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt={movie.name}
+            style={{ maxWidth: '100%', height: 'auto' }}
+            />
+            <div className="modal_content">
+                <p className="modal_details">
+                    {movie.release_date ? movie.release_date : movie.first_air_date}
+                </p>
+                <h2 className="modal_title">
+                    {movie.title ? movie.title : movie.name}
+                </h2>
+                <p className="modal_overview">평점 : {movie.vote_average}</p>
+                <p className="modal_overview">{movie.overview}</p>
+            </div>
+        </section>
     );
 };
 
 export default DetailPage;
+
+const StyledImage = styled.img`
+    max-width: 100%;
+    height: auto;
+`
