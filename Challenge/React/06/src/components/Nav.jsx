ef363b3155f2c3b61853704db50ae51a24922bac
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-24 10:49:16 +0900

    feat : 로그아웃 구현

--- Challenge/React/06/src/components/Nav.jsx
+++ Challenge/React/06/src/components/Nav.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components'
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from 'firebase/auth'
 import app from '../firebase';
 
 
 const Nav = () => {
     
     const [searchValue, setSearchValue] = useState('')
+    const [userData, setUserData] = useState({})
 
     const handleChange = (e) => {
         setSearchValue(e.target.value)
@@ -35,11 +36,21 @@ const Nav = () => {
         signInWithPopup(auth, provider)
             .then((result) => {
                 console.log(result);
+                setUserData(result.user)
             })
             .catch((error) => {
                 alert(error.message);
             })
     }
+
+    const handleLogOut = () => {
+        signOut(auth).then(() => {
+            setUserData({})
+        }).catch((error) => {
+            alert(error.message)
+        })
+    }
+
 
     return (
         <NavWrapper>
@@ -64,6 +75,20 @@ const Nav = () => {
                 />
             )
         }
+
+        {pathname !== "/" ? 
+        <SignOut>
+        <UserImg src={userData.photoURL} alt={userData.displayName}/>
+        <DropDown>
+            <span onClick={handleLogOut}>
+                로그아웃
+            </span>
+        </DropDown>
+        </SignOut>
+        :
+        null
+        }
+
         </NavWrapper>
     );
 };
@@ -83,6 +108,7 @@ const NavWrapper = styled.nav`
     z-index: 3;
 `
 
+//로그인
 const Logo = styled.a`
     padding: 0;
     width: 70px;
@@ -97,24 +123,24 @@ const Logo = styled.a`
 `
 
 const Input = styled.input`
-position: fixed;
-left: 50%;
-transform: translate(-50%, 0);
-background-color: rgba(0,0,0,0.5);
-border-radius: 5px;
-color: white;
-padding: 5px;
-border: 1px solid lightgray
+    position: fixed;
+    left: 50%;
+    transform: translate(-50%, 0);
+    background-color: rgba(0,0,0,0.5);
+    border-radius: 5px;
+    color: white;
+    padding: 5px;
+    border: 1px solid lightgray
 `
 
 const Login = styled.a`
-background-color: rgba(0,0,0,0.6);
-padding: 8px 16px;
-text-transform: uppercase;
-letter-spacing: 1.5px;
-border: 1px solid #f9f9f9;
-border-radius: 4px;
-transition: all 0.2s ease;
+    background-color: rgba(0,0,0,0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.2s ease;
 
 
 &:hover{
@@ -123,5 +149,45 @@ transition: all 0.2s ease;
     border-color: transparent;
 }
 `
+
+// 로그아웃
+const UserImg = styled.img`
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+`
+
+
+const DropDown = styled.div`
+    position: absolute;
+    top: 48px;
+    right: 0;
+    background: rgb(19, 19, 19);
+    border: 1px solid rgba(151, 151, 151, 0.34);
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+    padding: 10px;
+    font-size: 14px;
+    letter-spacing: 3px;
+    width: 100px;
+    opacity: 0;
+`
+
+const SignOut = styled.div`
+    position: relative;
+    height: 48px;
+    width: 48px;
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        ${DropDown} {
+            opacity: 1;
+            transition-duration: 1s;
+        }
+    }
+`;
 
 export default Nav;
