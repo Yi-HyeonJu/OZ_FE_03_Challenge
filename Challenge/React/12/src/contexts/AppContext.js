b4cd5e7138e6424a783c138cbe3df208c36cc8f0
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-27 16:55:19 +0900

    feat : Context 사용해서 데이터 제공하기

---
+++ Challenge/React/12/src/contexts/AppContext.js
@@ -0,0 +1,53 @@
+import { createContext, useReducer } from 'react'
+
+export const AppContext = createContext()
+
+export const AppReducer = (state, action) => {
+
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return {
+                ...state,
+                expense: [...state.expense, action.payload]
+            }
+        case 'DELETE_EXPENSE':
+            return {
+                ...state,
+                expenses: state.expenses.filter(
+                    (expense) => expense.id !== action.payload
+                )
+            }
+        case 'SET_BUDGET':
+            return {
+                ...state,
+                budget: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+const initialState = {
+    budget: 30000,
+    expenses: [
+        { id: crypto.randomUUID(), name: '간식비', cost: 2000 },
+        { id: crypto.randomUUID(), name: '교통비', cost: 10000 },
+        { id: crypto.randomUUID(), name: '구독비', cost: 5000 }
+    ]
+}
+
+export const AppContextProvider = (props) => {
+
+    //const [value, setValue] = useState('')
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    return(
+        <AppContext.Provider value={{
+            expenses: state.expenses,
+            budget: state.budget,
+            dispatch
+
+        }} {...props}/>
+    )
+}
--- Challenge/React/12/src/main.jsx
+++ Challenge/React/12/src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.main.css'
+import { AppContextProvider } from './contexts/AppContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <AppContextProvider value={{}}>
     <App />
-  </React.StrictMode>,
+  </AppContextProvider>
 )
