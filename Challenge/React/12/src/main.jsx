c2a0ac2767ebbc6b1d608edd5b1b1c1f68cd7d6d
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-27 19:04:53 +0900

    feat : 확장자 변경

--- Challenge/React/12/src/main.jsx
+++ Challenge/React/12/src/main.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.main.css'
-import { AppContextProvider } from './contexts/AppContext.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { AppContextProvider } from './contexts/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppContextProvider value={{}}>
