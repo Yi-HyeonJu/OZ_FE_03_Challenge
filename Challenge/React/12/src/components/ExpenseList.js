0a5488b9b137ee40061d94c3b4bf497d8f97c088
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-27 18:40:49 +0900

    feat : list 컴포넌트 작성

--- Challenge/React/12/src/components/ExpenseList.js
+++ Challenge/React/12/src/components/ExpenseList.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import ExpenseItem from './ExpenseItem';
 
 const ExpenseList = () => {
+    
+    const { expenses } = useContext(AppContext)
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <input
+            type='text'
+            className='form-control'
+            placeholder='검색하기...'
+            />
+            <ul className='list-group mt-3 mb-3'>
+                {expenses.map((expense) => {
+                    <ExpenseItem
+                    key={expense.id}
+                        id={expense.id}
+                        name={expense.name}
+                        cost={expense.cost}
+                    />
+                })}
+            </ul>
+        </>
     );
 };
 
