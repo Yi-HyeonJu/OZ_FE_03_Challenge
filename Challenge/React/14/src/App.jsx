4c756b123f181f2d26d8b64c0a3b01074d44fe6a
Yi-HyeonJu <guswwn1925@gmail.com>
2024-05-31 16:27:27 +0900

    feat : todo 만들기

--- Challenge/React/14/src/App.jsx
+++ Challenge/React/14/src/App.jsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { useState } from 'react'
+import { addTodo, deleteTodo, editTodo, toggleTodo } from './store/todoSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todos = useSelector(state => state.todos)
+
+  const [text, setText] = useState('')
+  const [editText, setEditText] = useState('')
+  const [editId, setEditId] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const todoSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim() !== ''){
+      dispatch(
+        addTodo(text)
+      )
+      setText('')
+    }
+  }
+
+  const todoDelete = (id) => {
+    dispatch(
+      deleteTodo(id)
+    )
+  }
+
+  const todoEdit = (id, text) => {
+    setEditId(id)
+    setEditText(text)
+  }
+
+  const editCancle = () => {
+    setEditId(null)
+    setEditText('')
+  }
+
+  const editSave = () => {
+    if(editText.trim() !== '') {
+      dispatch(
+        editTodo({
+          id: editId,
+          newText: editText
+        })
+      )
+      setEditId(null)
+      setEditText('')
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <form onSubmit={todoSubmit}>
+        <input value={text} onChange={e => setText(e.target.value)}/>
+        <button
+          type='submit'
+        >할 일 추가하기</button>
+      </form>
+      <ul>
+        {todos.map(
+          todo => (
+            <li key={todo.id}>
+              {todo.id === editId
+              ?
+            <>
+              <input type='text' value={editText} onChange={(e) => setEditText(e.target.value)}/>
+              <button onClick={editSave}>저장</button>
+              <button onClick={editCancle}>취소</button>
+            </>
+              :
+            <>
+              <input type='checkbox'
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}/>
+              <span>
+                {todo.text}
+              </span>
+              <button onClick={() => todoEdit(todo.id, todo.text)}>수정</button>
+              <button onClick={() => todoDelete(todo.id)}>삭제</button>
+            </>
+        }
+            </li>
+          )
+        )}
+      </ul>
+    </div>
   )
 }
 
